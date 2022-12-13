e6d1d932c4f084e914cbec64b1afcb955d1af363
evlazarev007 <115842128+evlazarev007@users.noreply.github.com>
2022-12-14 02:37:38 +0600

    1

--- game.sh
+++ game.sh
@@ -1,42 +1,51 @@
+#!/bin/bash
+
 echo -e "\n NumberJack \n"
-ch=0
-while [ $ch -ne 3 ]; do
-    echo  "  
-         PLAY : Hit 1 and enter.
-         HELP : Hit 2 and enter.
-         EXIT : Hit 3 and enter.
-         "
-    read -p "Enter your choice : " ch
-    if [ $ch -eq 1 ];then
-    x=0 ;c=0 ;p=0
-    read -p "Enter any number between 0 and 9 : " n
-    while [ $c -eq 0 ];
-    do
-        x=11; r=($(shuf -i 0-9 -n 10))
-        echo "${r[@]} "; for i in {1..10}; do
-            a[$i]=$i
+choice=0
+
+while [ "$choice" -ne 3 ]; do
+  echo  "
+  PLAY : Hit 1 and enter.
+  HELP : Hit 2 and enter.
+  EXIT : Hit 3 and enter.
+  "
+  read -r -p "Enter your choice : " choice
+  if [ "$choice" -eq 1 ];then
+    x=0
+    cancel=0
+    points=0
+    read -r -p "Enter any number between 0 and 9 : " n
+      while [ "$cancel" -eq 0 ]; do
+        mapfile -t r < <(shuf -i 0-9 -n 10)
+        echo "${r[@]}";
+        for i in {1..10}; do
+          a[$i]="$i"
         done
-        echo "${a[@]} "
-        read -t 5 -p "Enter the index of your number : " x
-    if [[ $? -gt 128 ]]; then
-    c=1
-    break
-    fi
-if [ ${r[$(($x))-1]} -eq $n ];then
-            echo "Great"
-    ((p=p+1))
-        else
-            c=1
-            break
+        echo "${a[@]}"
+        read -r -t 5 -p "Enter the index of your number : " x
+
+        if [[ "$?" -gt 128 ]]; then
+          cancel=1
+          break
         fi
-    done
-    elif [ $ch -eq 2 ];then
-        echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
-else
-        break
-fi
-if [ $c -eq 1 ];then
-            echo -e "\nGAME OVER\n"
-            echo "You scored $p points"
-fi
-        done
+
+        if [ "${r[$x-1]}" -eq "$n" ];then
+          echo "Great"
+          ((points=points+1))
+        else
+          cancel=1
+          break
+        fi
+      done
+  elif [ "$choice" -eq 2 ];then
+    echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
+  else
+    break
+  fi
+
+  if [ "$cancel" -eq 1 ];then
+    echo -e "\nGAME OVER\n"
+    echo "You scored $points points"
+  fi
+
+done
